a6c7078f0dfa80d26d113b61a41e13ec3165d824
oussamarhouch <oussamarhouch@gmail.com>
2023-06-04 00:22:46 +0100

    safe delete

--- lib/model/symbol_model.dart
+++
@@ -1,6 +0,0 @@
-class SymbolModel {
-  final String symbol;
-  final String price;
-
-  SymbolModel({required this.symbol, required this.price});
-}
--- lib/service/newsapi/api_key.dart
+++
@@ -1,5 +0,0 @@
-class ApiKeys {
-  static const String baseURL =
-      "https://newsapi.org/v2/top-headlines?country=us";
-  static const String apiKeyId = "---YOUR API KEY---";
-}
