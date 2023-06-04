574ad442430a5b8eb9c90387cdf7d7a38ba4bb64
oussamarhouch <oussamarhouch@gmail.com>
2023-06-04 01:37:33 +0100

    profile API

--- server/src/main/java/com/cryptoApp/server/controllers/AuthController.java
+++ server/src/main/java/com/cryptoApp/server/controllers/AuthController.java
@@ -9,16 +9,13 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cryptoApp.server.models.ERole;
 import com.cryptoApp.server.models.Role;
@@ -121,5 +118,14 @@ public class AuthController {
 		userRepository.save(user);
 
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+
+	@GetMapping("/profile")
+	public ResponseEntity<?> getUserProfile(Authentication authentication) {
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+		User user = userRepository.findById(userDetails.getId())
+				.orElseThrow(() -> new RuntimeException("Error: User not found."));
+
+		return ResponseEntity.ok(user);
 	}
 }
